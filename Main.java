5118980bfbff4cbfc04fce81104c60dfac9d9a6a
Harry Leung <harrylungs91@gmail.com>
2020-08-13 11:03:22 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,6 @@ public class Main
 		System.out.println("I love computers and programing and I want to major in computer science.");
 		System.out.println("My dream school is UC Berkeley.");
 		System.out.println("I like to play League of Legends, as well as social deduction games, such as Among Us or Resistance.");
-		System.out.println("Mr. Castleman has a 5 in his discard ;).");
+		System.out.println("Mr. Castleman has a 5 in his discard.");
 	}
 }
