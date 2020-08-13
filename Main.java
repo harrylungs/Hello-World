f6474de7c2b6619b08f1a8e9eb5792b9f1cc7a0a
Harry Leung <harrylungs91@gmail.com>
2020-08-13 10:37:09 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("One of my hobbies is working out.");
+		System.out.println("I love computers and programing and I want to major in computer science.");
+		System.out.println("My dream school is UC Berkeley.");
+		System.out.println("I like to play League of Legends, as well as social deduction games, such as Among Us or Resistance.");
+		System.out.println("Mr. Castleman has a 5 in his discard ;).");
+	}
+}
